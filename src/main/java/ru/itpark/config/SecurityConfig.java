949e594c89fd4ffb732a445398147e71fedd2004
Student21 <noname@ya.ru>
2018-05-26 16:39:24 +0300

    Custom login page

---
+++ src/main/java/ru/itpark/config/SecurityConfig.java
@@ -0,0 +1,22 @@
+package ru.itpark.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.authorizeRequests()
+        .anyRequest().authenticated()
+        .and()
+        .formLogin().loginPage("/login").permitAll()
+        .and()
+        .logout().permitAll();
+  }
+}
